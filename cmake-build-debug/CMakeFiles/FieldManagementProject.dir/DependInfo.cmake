2809a60128a3d3399c782649b68a8bd2c24e0301
yagel try11 <yagelbe@ac.sce.ac.il>
2024-02-28 22:10:40 +0200

    new yagel

---
+++ cmake-build-debug/CMakeFiles/FieldManagementProject.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/smada/CLionProjects/FieldManagementProject/DataAccessLayer.cpp" "CMakeFiles/FieldManagementProject.dir/DataAccessLayer.cpp.obj" "gcc" "CMakeFiles/FieldManagementProject.dir/DataAccessLayer.cpp.obj.d"
+  "C:/Users/smada/CLionProjects/FieldManagementProject/Date.cpp" "CMakeFiles/FieldManagementProject.dir/Date.cpp.obj" "gcc" "CMakeFiles/FieldManagementProject.dir/Date.cpp.obj.d"
+  "C:/Users/smada/CLionProjects/FieldManagementProject/Field.cpp" "CMakeFiles/FieldManagementProject.dir/Field.cpp.obj" "gcc" "CMakeFiles/FieldManagementProject.dir/Field.cpp.obj.d"
+  "C:/Users/smada/CLionProjects/FieldManagementProject/Manager.cpp" "CMakeFiles/FieldManagementProject.dir/Manager.cpp.obj" "gcc" "CMakeFiles/FieldManagementProject.dir/Manager.cpp.obj.d"
+  "C:/Users/smada/CLionProjects/FieldManagementProject/Menu.cpp" "CMakeFiles/FieldManagementProject.dir/Menu.cpp.obj" "gcc" "CMakeFiles/FieldManagementProject.dir/Menu.cpp.obj.d"
+  "C:/Users/smada/CLionProjects/FieldManagementProject/Order.cpp" "CMakeFiles/FieldManagementProject.dir/Order.cpp.obj" "gcc" "CMakeFiles/FieldManagementProject.dir/Order.cpp.obj.d"
+  "C:/Users/smada/CLionProjects/FieldManagementProject/Player.cpp" "CMakeFiles/FieldManagementProject.dir/Player.cpp.obj" "gcc" "CMakeFiles/FieldManagementProject.dir/Player.cpp.obj.d"
+  "C:/Users/smada/CLionProjects/FieldManagementProject/Validation.cpp" "CMakeFiles/FieldManagementProject.dir/Validation.cpp.obj" "gcc" "CMakeFiles/FieldManagementProject.dir/Validation.cpp.obj.d"
+  "C:/Users/smada/CLionProjects/FieldManagementProject/main.cpp" "CMakeFiles/FieldManagementProject.dir/main.cpp.obj" "gcc" "CMakeFiles/FieldManagementProject.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/FieldManagementProject.dir/Field.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/FieldManagementProject.dir/Field.cpp.obj: \
+ C:\Users\smada\CLionProjects\FieldManagementProject\Field.cpp
