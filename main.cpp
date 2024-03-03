28c7d5fa3f51bcf3e1886790c073eca2fc84b579
IMrMoon <sgvchen12@gmail.com>
2024-03-03 21:48:43 +0200

    finish all

--- main.cpp
+++ main.cpp
@@ -10,6 +10,13 @@ using namespace SQLite;
 
 int main(){
     Database db("FieldManagement.db", OPEN_READWRITE|OPEN_CREATE);
+    cout << "Field Management Project by: \n"
+            "Segev Chen 322433400 \n"
+            "Yinon Alfasi 208810374 \n"
+            "Yagel batito 318271863 \n"
+            "Noa Kraif 208595157 \n"
+            "Yael Berko 213435399 \n"
+            "Git link: https://github.com/IMrMoon/FieldManagementProject.git" << endl;
     first_menu();
 return 0;
 }
